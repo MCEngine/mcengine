1631fb3adb88d2d8042b0c2f1f0bb8cbec5a2a9f
JetsadaWijit <jetsadawijit@outlook.com>
2025-02-21 09:40:56 +0000

    Add and Fix comments

--- mcengine-api/src/main/java/io/github/mcengine/api/mcengine/database/MCEngineApiDatabase.java
+++ mcengine-api/src/main/java/io/github/mcengine/api/mcengine/database/MCEngineApiDatabase.java
@@ -3,9 +3,25 @@ package io.github.mcengine.api.mcengine.database;
 import io.github.mcengine.api.mcengine.database.mysql.MCEngineApiMYSQL;
 import io.github.mcengine.api.mcengine.database.sqlite.MCEngineApiSQLITE;
 
+/**
+ * The {@code MCEngineApiDatabase} class is responsible for handling database connections
+ * by selecting the appropriate database provider based on the specified database type.
+ */
 class MCEngineApiDatabase {
     private final MCEngineApiDatabaseProvider databaseProvider;
 
+    /**
+     * Constructs a new {@code MCEngineApiDatabase} instance with the specified database type and connection arguments.
+     *
+     * @param dbType The type of database to use. Supported values: "sqlite", "mysql".
+     * @param args   The arguments required for the database connection:
+     *               <ul>
+     *                   <li>For SQLite: {@code args[0]} - The database file path.</li>
+     *                   <li>For MySQL: {@code args[0]} - Host, {@code args[1]} - Database name,
+     *                       {@code args[2]} - Username, {@code args[3]} - Password, {@code args[4]} - Port.</li>
+     *               </ul>
+     * @throws IllegalArgumentException if an unsupported database type is provided.
+     */
     public MCEngineApiDatabase(String dbType, String[] args) {
         switch (dbType.toLowerCase()) {
             case "sqlite":
@@ -21,6 +37,11 @@ class MCEngineApiDatabase {
         }
     }
 
+    /**
+     * Retrieves the database provider instance.
+     *
+     * @return The initialized database provider.
+     */
     public MCEngineApiDatabaseProvider getDatabaseProvider() {
         return databaseProvider;
     }
--- server/spigotmc/src/main/java/io/github/mcengine/spigotmc/mcengine/MCEngine.java
+++ server/spigotmc/src/main/java/io/github/mcengine/spigotmc/mcengine/MCEngine.java
@@ -2,11 +2,23 @@ package io.github.mcengine.spigotmc.mcengine;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * Main class for the MCEngine plugin.
+ * This class extends JavaPlugin and manages the plugin lifecycle.
+ */
 public class MCEngine extends JavaPlugin {
 
+    /**
+     * Called when the plugin is enabled.
+     * This method is executed when the server starts or when the plugin is reloaded.
+     */
     @Override
     public void onEnable() {}
 
+    /**
+     * Called when the plugin is disabled.
+     * This method is executed when the server stops or when the plugin is reloaded.
+     */
     @Override
     public void onDisable() {}
 }
